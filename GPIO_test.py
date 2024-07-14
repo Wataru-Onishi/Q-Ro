a90747148bc55ecba9c93c7422cde4d93afa2cdc
Wataru Onishi <wataru71017@gmail.com>
2024-07-14 16:23:42 +0900

    Update GPIO_test.py

--- GPIO_test.py
+++ GPIO_test.py
@@ -4,8 +4,8 @@ import time
 # GPIO番号指定の準備
 GPIO.setmode(GPIO.BCM)
 
-# 指定されたGPIOピン
-pins = [16, 19, 21, 26]
+# 10番以降のGPIOピン
+pins = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
 
 # GPIOピンの設定
 for pin in pins:
@@ -14,11 +14,13 @@ for pin in pins:
 try:
     while True:
         # 各ピンの状態を読み取り、表示
+        print("現在のGPIOピンの状態:")
         for pin in pins:
             state = GPIO.input(pin)
             print(f"GPIO{pin}: {'HIGH' if state else 'LOW'}")
         
         # 更新速度（ここでは1秒毎に更新）
+        print("-----")
         time.sleep(1)
 
 except KeyboardInterrupt:
