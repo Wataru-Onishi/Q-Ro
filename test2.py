07a092482aaef04787484620c464b126e1814dfb
Wataru Onishi <wataru71017@gmail.com>
2024-07-21 16:06:02 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -66,11 +66,11 @@ CURRENT_LIMIT_LOW = 4
 current_limit = CURRENT_LIMIT_HIGH
 
 # Position and velocity settings
-standard_position = 100
+standard_position = 1024
 forward_velocity = 400
 backward_velocity = -400
 turning_velocity = 100
-new_goal_position = 500
+new_goal_position = 2048
 
 # Mode settings
 MANUAL_MODE = 0
