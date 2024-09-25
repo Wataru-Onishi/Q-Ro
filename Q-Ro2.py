5c499764e6b46fb58b512040edba1c92b17fc8b1
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-09-25 15:01:38 +0900

    Update Q-Ro2.py

--- Q-Ro2.py
+++ Q-Ro2.py
@@ -87,15 +87,15 @@ BACKWARD_DIRECTION = {
 
 RIGHT_TURN_DIRECTION = {
     1: TURNING_SPEED,   # Speed for right turn
-    2: 0*TURNING_SPEED,   # Speed for ID 2
+    2: TURNING_SPEED,   # Speed for ID 2
     3: TURNING_SPEED,   # Speed for ID 3
-    4: 0*TURNING_SPEED    # Speed for right turn
+    4: TURNING_SPEED    # Speed for right turn
 }
 
 LEFT_TURN_DIRECTION = {
-    1: 0*-TURNING_SPEED,  # Speed for left turn
+    1: -TURNING_SPEED,  # Speed for left turn
     2: -TURNING_SPEED,  # Speed for ID 2
-    3: 0*-TURNING_SPEED,  # Speed for ID 3
+    3: -TURNING_SPEED,  # Speed for ID 3
     4: -TURNING_SPEED   # Speed for left turn
 }
 
