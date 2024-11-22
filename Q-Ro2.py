ece3d16437e007f5eaf22a3d57bad3716ab4e1a4
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-11-22 15:23:05 +0900

    Update Q-Ro2.py

--- Q-Ro2.py
+++ Q-Ro2.py
@@ -72,9 +72,9 @@ current_mode = MANUAL_MODE
 
 # Define motor direction constants for forward, backward, left, and right turns
 FORWARD_DIRECTION = {
-    1: velocity_value+50,
+    1: velocity_value+10,
     2: -velocity_value,
-    3: velocity_value+50,
+    3: velocity_value+10,
     4: -velocity_value
 }
 
